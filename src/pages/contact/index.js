a783f09d7367916b75c290ceda76b4bca873f349
wenzhi <2269082753@qq.com>
2020-03-28 15:46:24 +0800

    Fix bugs

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -6,7 +6,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
 import 'antd/dist/antd.css';
-import { } from 'antd';
+import {} from 'antd';
 
 import SideBar from '../sidebar';
 
@@ -18,8 +18,8 @@ function About() {
       <div className={styles.connectMe}>
         <div className={styles.callWe}>
           <img src={useBaseUrl('img/联系我-banner.png')} alt="22" />
-          <h1>英荔播课</h1>
-          <small>为互联网时代企业培训变革赋能</small>
+          {/* <h1>英荔播课</h1>
+          <small>为互联网时代企业培训变革赋能</small> */}
         </div>
         <div className={styles.callTit}>
           <h4>联系我们</h4>
@@ -30,7 +30,7 @@ function About() {
                 <p>地址：广州市天河区天河北路 233 号中信广场写字楼 1005 室</p>
                 <p>
                   电话：
-                  <a href="tel:400-931-8118" style={{ color: 'inherit' }}>
+                  <a href="tel:400-931-8118" style={{color: 'inherit'}}>
                     400-931-8118
                   </a>
                   （周一至周五 9:00 - 18:00）
@@ -44,20 +44,20 @@ function About() {
                 <p className={styles.cooperate}>
                   <span class={styles.cooperateItem}>
                     黄小姐：
-                    <a href="tel:156 261 02305" style={{ color: 'inherit' }}>
-                      156 261 02305
+                    <a href="tel:156 261 02305" style={{color: 'inherit'}}>
+                      156 2610 2305
                     </a>
                   </span>
                   <span class={styles.cooperateItem}>
                     廖小姐：
-                    <a href="tel:139 289 68545" style={{ color: 'inherit' }}>
-                      139 289 68545
+                    <a href="tel:139 289 68545" style={{color: 'inherit'}}>
+                      139 2896 8545
                     </a>
                   </span>
                   <span class={styles.cooperateItem}>
                     孙小姐：
-                    <a href="tel:153 188 61202" style={{ color: 'inherit' }}>
-                      153 188 61202
+                    <a href="tel:153 188 61202" style={{color: 'inherit'}}>
+                      153 1886 1202
                     </a>
                   </span>
                 </p>
