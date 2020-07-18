86c516e06c750c65434fd2c094da757e91199963
LiuGuinv <997207868@qq.com>
2020-07-18 09:38:48 +0800

    modified download url

--- src/pages/download/index.js
+++ src/pages/download/index.js
@@ -6,7 +6,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
 import 'antd/dist/antd.css';
-import { } from 'antd';
+import {} from 'antd';
 
 import SideBar from '../sidebar';
 const download = url => {
@@ -16,9 +16,9 @@ const downloadDesk = (key, e) => {
   let downloadUrl = '';
   let baseUrl = 'https://yingliboke.oss-cn-shenzhen.aliyuncs.com/app/';
   if (key === 'win') {
-    downloadUrl = baseUrl + '英荔播课%20Setup%202.3.3.exe';
+    downloadUrl = baseUrl + '英荔播课%20Setup%202.5.1.exe';
   } else if (key === 'mac') {
-    downloadUrl = baseUrl + 'www英荔播课-2.3.3.dmg';
+    downloadUrl = baseUrl + 'www英荔播课-2.5.1.dmg';
   }
   // 创建a标签，触发点击
   const a = document.createElement('a');
@@ -49,7 +49,7 @@ function Download() {
                     className={styles.downIcon}
                     onClick={() =>
                       download(
-                        'https://yingliboke.oss-cn-shenzhen.aliyuncs.com/android/www%E8%8B%B1%E8%8D%94%E6%92%AD%E8%AF%BE2.3.4-05-21_1853_234_jiagu_sign.apk'
+                        'https://yingliboke.oss-cn-shenzhen.aliyuncs.com/android/www%E8%8B%B1%E8%8D%94%E6%92%AD%E8%AF%BE2.5.1-07-17_1526_251_jiagu_sign.apk',
                       )
                     }>
                     <img
@@ -60,7 +60,7 @@ function Download() {
                   <small
                     onClick={() =>
                       download(
-                        'https://yingliboke.oss-cn-shenzhen.aliyuncs.com/android/www%E8%8B%B1%E8%8D%94%E6%92%AD%E8%AF%BE2.3.4-05-21_1853_234_jiagu_sign.apk'
+                        'https://yingliboke.oss-cn-shenzhen.aliyuncs.com/android/www%E8%8B%B1%E8%8D%94%E6%92%AD%E8%AF%BE2.5.1-07-17_1526_251_jiagu_sign.apk',
                       )
                     }>
                     点击或扫描二维码下载
